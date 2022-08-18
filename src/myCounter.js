5d026bb9d53404bb0dd73c7cf0d78b737f0a3e8c
Priscila <priscilasartori2@gmail.com>
2022-08-18 13:50:54 -0300

    Resolução item 08

--- src/myCounter.js
+++ src/myCounter.js
@@ -15,11 +15,11 @@
   myCounter() // Retorna: [0, 2, 3, 1, 2, 3, 2, 2, 3, 3, 2, 3];
 */
 
-const myCounter = () => {
+const myCounter = (array) => {
   var myArray = [];
-  for (var counter = 0; counter <= 3; counter += 1) {
+  for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
+    for (let counter = 2; counter <= 3; counter += 1) {
       myArray.push(counter);
     }
   }

d48d4fe213abc8ab66635f838d3c5725af7e5e26
Priscila <priscilasartori2@gmail.com>
2022-10-21 22:37:22 -0300

    Requisito 7

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,14 +15,13 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
     */
 
-   const createStudent = (name) => {
-      const goodPerson = 'Eita pessoa boa!';
-  
-      const estudante = {
-        name,
-        feedback: () => goodPerson,
-    }; 
-    return estudante;
-  }; 
+const createStudent = (name) => {
+  const goodPerson = 'Eita pessoa boa!';
+  const estudante = {
+    name,
+    feedback: () => goodPerson,
+  };
+  return estudante;
+};
 
-    module.exports = createStudent;
+module.exports = createStudent;
